0cdd055dd8d108a7b0bd287aecc685c50c69300b
Karen Minasyan <kminasyantricking@gmail.com>
2023-01-07 20:12:06 +0400

    output meal details

--- components/MealItem/index.js
+++ components/MealItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, Pressable, Text, View, StyleSheet, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import MealDetails from '../MealDetails';
 
 const MealItem = ({ id, title, imageUrl, duration, complexity, affordability }) => {
 	const navigation = useNavigation();
@@ -21,11 +22,11 @@ const MealItem = ({ id, title, imageUrl, duration, complexity, affordability }) 
 						<Image source={{ uri: imageUrl }} style={styles.image}/>
 						<Text style={styles.title}>{title}</Text>
 					</View>
-					<View style={styles.details}>
-						<Text style={styles.detailItem}>{duration}m</Text>
-						<Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-						<Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-					</View>
+					<MealDetails
+						duration={duration}
+						complexity={complexity}
+						affordability={affordability}
+					/>
 				</View>
 			</Pressable>
 		</View>
@@ -63,14 +64,4 @@ const styles = StyleSheet.create({
 		fontSize: 18,
 		margin: 8,
 	},
-	details: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'center',
-		padding: 8,
-	},
-	detailItem: {
-		marginHorizontal: 4,
-		fontSize: 12,
-	}
 });
--- screens/MealDetailScreen/index.js
+++ screens/MealDetailScreen/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { MEALS } from '../../data/dummy-data';
+import MealDetails from '../../components/MealDetails';
 
 const MealDetailScreen = ({ route }) => {
 	const mealId = route.params.mealId;
+
+	const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
 	return (
 		<View>
-			<Text>meal detail screen {mealId}</Text>
+			<Image source={{ uri: selectedMeal.imageUrl }}/>
+			<Text>{selectedMeal.title}</Text>
+			<MealDetails
+				affordability={selectedMeal.affordability}
+				complexity={selectedMeal.complexity}
+				duration={selectedMeal.duration}
+			/>
+			<Text>Ingredients</Text>
+			{selectedMeal.ingredients.map(ingredient => (
+				<Text key={ingredient}>{ingredient}</Text>
+			))}
+			<Text>Steps</Text>
+			{selectedMeal.steps.map(step => (
+				<Text key={step}>{step}</Text>
+			))}
 		</View>
 	);
 };
