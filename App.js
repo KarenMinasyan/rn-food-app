d351dd43319b06d87bf62fd018ba963ce1ccdf2c
Karen Minasyan <kminasyantricking@gmail.com>
2023-01-08 14:26:51 +0400

    combine stack with drawer navigation

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
+import 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import { createDrawerNavigator } from '@react-navigation/drawer';
-//import { Ionicons } from '@expo/vector-icons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
-//import WelcomeScreen from './screens/WelcomeScreen';
-//import UserScreen from './screens/UserScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
-//const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+	return (
+		<Drawer.Navigator>
+			<Drawer.Screen name='Categories' component={CategoriesScreen} />
+			<Drawer.Screen name='Favorites' component={FavoritesScreen} />
+		</Drawer.Navigator>
+	)
+}
 
 export default function App() {
 	return (
@@ -19,7 +27,6 @@ export default function App() {
 			<StatusBar style='light'/>
 			<NavigationContainer>
 				<Stack.Navigator
-					initialRouteName='MealsCategories'
 					screenOptions={{
 						headerStyle: { backgroundColor: '#351401' },
 						headerTintColor: 'white',
@@ -27,49 +34,14 @@ export default function App() {
 					}}
 				>
 					<Stack.Screen
-						name='MealsCategories'
-						component={CategoriesScreen}
-						options={{ title: 'All Categories' }}
+						name='Drawer'
+						component={DrawerNavigator}
+						options={{ headerShown: false }}
 					/>
-					<Stack.Screen
-						name='MealsOverview'
-						component={MealsOverviewScreen}
-						// options={({ route, navigation }) => {
-						// 	const catId = route.params.categoryId;
-						// 	return { title: catId }
-						// }}
-					/>
-					<Stack.Screen
-						name='MealDetail'
-						component={MealDetailScreen}
-						// options={{ headerRight: () => <Button title='Tap me!' onPress={} /> }}
-					/>
+					<Stack.Screen name='MealsOverview' component={MealsOverviewScreen}/>
+					<Stack.Screen name='MealDetail' component={MealDetailScreen}/>
 				</Stack.Navigator>
 			</NavigationContainer>
-			{/*<NavigationContainer>*/}
-			{/*<Drawer.Navigator*/}
-			{/*	screenOptions={{*/}
-			{/*		headerStyle: { backgroundColor: '#3c0a6b' },*/}
-			{/*		headerTintColor: 'white',*/}
-			{/*		drawerActiveBackgroundColor: '#f0e1ff',*/}
-			{/*		drawerActiveTintColor: '#3c0a6b',*/}
-			{/*		//drawerStyle: { backgroundColor: '#ccc' }*/}
-			{/*	}}*/}
-			{/*>*/}
-			{/*	<Drawer.Screen*/}
-			{/*		name='Welcome' component={WelcomeScreen}*/}
-			{/*		options={{*/}
-			{/*			drawerLabel: 'Welcome Screen',*/}
-			{/*     drawerIcon: ({color, size}) => <Ionicons name='home' color={color} size={size} />  */}
-			{/*		}}*/}
-			{/*	/>*/}
-			{/*	<Drawer.Screen name='User' component={UserScreen}
-			{/*		options={{*/}
-			{/*     drawerIcon: ({color, size}) => <Ionicons name='person' color={color} size={size} />  */}
-			{/*		}}*/}
-			{/*/>*!/*/}
-			{/*</Drawer.Navigator>*/}
-			{/*</NavigationContainer>*/}
 		</>
 	);
 }
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const FavoritesScreen = () => {
+	return (
+			<Text>the favorites screen</Text>
+	);
+};
+
+export default FavoritesScreen;
